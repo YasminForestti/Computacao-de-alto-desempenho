1564558dc0bc94fdb203a110ed0240bbe59938b3
Yasminforestti <y.f.romeiro@poli.ufrj.br>
2023-05-12 21:26:00 -0300

    d

--- Produto.f90
+++ Produto.f90
@@ -1,29 +1,30 @@
 PROGRAM matriz_vetor_multiplication
    IMPLICIT NONE
-    INTEGER :: i, j, ROWS, COLS, inverso, unit, ierr, arg_count
-    CHARACTER(len = 40) :: r_char
-    CHARACTER(len = 40) :: c_char
+    INTEGER :: num,i, j, ROWS, COLS, inverso, unit, ierr, arg_count,max
+    CHARACTER(len = 40) :: max_char
     CHARACTER(len = 40) :: inverso_char
     REAL :: start_time, end_time, time_elapsed
     REAL, DIMENSION(:,:), ALLOCATABLE :: A
     REAL, DIMENSION(:), ALLOCATABLE :: b, x
-    CHARACTER(len=30) :: filename
+    CHARACTER(len=30) :: filename = "dados_fortran_0.csv"
 
     arg_count = command_argument_count()
 
-    if (arg_count < 3) then
-        write(*,*) "Usage: ./program ROWS COLS inverso"
+    if (arg_count < 2) then
+        write(*,*) "Usage: ./program max inverso"
         stop
     end if
 
-    call get_command_argument(1,r_char)
-    call get_command_argument(2,c_char)
-    call get_command_argument(3,inverso_char)
+    call get_command_argument(1,max_char)
+    call get_command_argument(2,inverso_char)
 
-    read(r_char,*) ROWS
-    read(c_char,*) COLS
-    read(inverso_char,*) inverso
- 
+    read(max_char,*) max
+    read(inverso_char,*) inverso    
+    
+    do num = 1, max, 10
+        ROWS = num
+        COLS = num
+
     ALLOCATE(A(ROWS,COLS))
     ALLOCATE(b(ROWS))
     ALLOCATE(x(COLS))
@@ -53,14 +54,14 @@ PROGRAM matriz_vetor_multiplication
     CALL CPU_TIME(end_time)
     time_elapsed = end_time - start_time
 
-    write(filename, "(a)") "dados_fortran.csv"
-    open(newunit=unit, file=trim(filename), status='old', action='write', iostat=ierr)
-
-
+    
+    open(newunit=unit, file=trim(filename), status='old', access='append', iostat=ierr)
+    
     write(unit, "(i0,';',i0,';',f5.2,';',i0)") ROWS, COLS, time_elapsed, inverso
     close(unit)
-
     DEALLOCATE(A)
     DEALLOCATE(b)
     DEALLOCATE(x)
+
+    end do
 END PROGRAM matriz_vetor_multiplication
